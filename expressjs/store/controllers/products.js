9c5dd3cad2b960d124be8eea46c3c7d598669b20
Harsh Modhera <hkmodhera@gmail.com>
2024-01-01 03:11:55 -0600

    added sort functionality to get query

--- expressjs/store/controllers/products.js
+++ expressjs/store/controllers/products.js
@@ -1,14 +1,22 @@
 const Product = require("../models/Product");
 
 const getAll = async (req, res) => {
-  const { name, featured, company } = req.query;
+  const { name, featured, company, sort } = req.query;
   const queryObj = {};
 
   if (name) queryObj.name = { $regex: name, $options: "i" };
   if (featured) queryObj.featured = featured == "true" ? true : false;
   if (company) queryObj.company = company;
 
-  const products = await Product.find(queryObj);
+  let result = Product.find(queryObj);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    // default sort
+    result = result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ meta: { count: products.length }, data: products });
 };
 
