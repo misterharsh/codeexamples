6090345eea77d300fc80006702b8d10c9545fd1d
Harsh Modhera <hkmodhera@gmail.com>
2024-01-03 21:43:08 -0600

    auth middleware

--- expressjs/jobs/index.js
+++ expressjs/jobs/index.js
@@ -6,6 +6,7 @@ const app = express();
 // connect db
 const connectDB = require("./db/connect");
 
+const authMiddleware = require("./middleware/authentication");
 // routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -19,7 +20,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
