823f5561a1b866374dfb112fe60e0176f037d435
Harsh Modhera <hkmodhera@gmail.com>
2024-01-06 00:48:26 -0600

    jobs done

--- expressjs/jobs/controllers/jobs.js
+++ expressjs/jobs/controllers/jobs.js
@@ -1,21 +1,62 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAll = async (req, res) => {
-  res.status(200).send("/register");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ meta: { count: jobs.length }, data: jobs });
 };
 
 const getById = async (req, res) => {
-  res.status(200).send("/login");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findById({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const save = async (req, res) => {
-  res.status(200).send("/login");
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const update = async (req, res) => {
-  res.status(200).send("/login");
+  const {
+    user: { userId },
+    params: { id: jobId },
+    body: { company, position },
+  } = req;
+
+  if (company == "" || position == "") {
+    throw new BadRequestError("Missing body properties");
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const remove = async (req, res) => {
-  res.status(200).send("/login");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findByIdAndDelete({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
